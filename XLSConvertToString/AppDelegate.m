2988953cc52270dafba68cfc793417899c138444
lym <lwb374402328@gmail.com>
2023-01-16 15:14:20 +0800

    feat: demo

--- XLSConvertToString/AppDelegate.m
+++ XLSConvertToString/AppDelegate.m
@@ -16,8 +16,13 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [NSApp setDelegate:self];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(closeWindow) name:NSWindowWillCloseNotification object:nil];
 }
 
+- (void)closeWindow {
+    [NSApp terminate:self];
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
--- XLSConvertToString/ViewController.m
+++ XLSConvertToString/ViewController.m
@@ -8,63 +8,150 @@
 
 #import "ViewController.h"
 #import "DHxlsReader.h"
-@interface ViewController()
-@property (weak) IBOutlet NSTextField *ecxelPathField;
-@property (weak) IBOutlet NSTextField *stringPathField;
 
+NSString *const excelPathStoreKey = @"excelPathStoreKey";
+NSString *const stringPathStoreKey = @"stringPathStoreKey";
+NSString *const projectResourceKey = @"projectResourceKey";
+NSString *const langKeysKey = @"langKeysKey";
+
+@interface ViewController()
+@property (weak) IBOutlet NSTextField *excelPathField;
+@property (weak) IBOutlet NSTextField *stringPathField;
+@property (weak) IBOutlet NSTextField *projectStringDirPathField;
+@property (weak) IBOutlet NSTextField *langKeysField;
+
+@property (nonatomic, strong) NSArray *langKeys;
 @end
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.ecxelPathField.enabled = NO;
-    self.stringPathField.enabled = NO;
+    self.excelPathField.enabled = NO;
+//    self.stringPathField.enabled = NO;
+//    self.projectStringDirPathField.enabled = NO;
+    [self.stringPathField setFocusRingType:NSFocusRingTypeNone];
+    [self.projectStringDirPathField setFocusRingType:NSFocusRingTypeNone];
+    [self.langKeysField setFocusRingType:NSFocusRingTypeNone];
     // Do any additional setup after loading the view.
+    
+    [self setupData];
+    [self restoreBeforeConfigs];
 }
 
-- (IBAction)excelPathSeleted:(NSButton *)sender {
+#pragma mark - data
+- (void)setupData{
+    self.langKeys = @[@"en", @"es", @"de", @"fr", @"tr", @"it"];
+    self.langKeysField.stringValue = [self.langKeys componentsJoinedByString:@","];
+    
+}
+
+#pragma mark - restore
+- (void)restoreBeforeConfigs{
+    NSString *excelPathBefore = [[NSUserDefaults standardUserDefaults] objectForKey:excelPathStoreKey];
+    if (excelPathBefore.length > 0){
+        self.excelPathField.stringValue = excelPathBefore;
+    }
+    
+    NSString *stringPathBefore = [[NSUserDefaults standardUserDefaults] objectForKey:stringPathStoreKey];
+    if (stringPathBefore.length > 0){
+        self.stringPathField.stringValue = stringPathBefore;
+    }
+    
+    NSString *projectPathBefore = [[NSUserDefaults standardUserDefaults] objectForKey:projectResourceKey];
+    if (projectPathBefore.length > 0){
+        self.projectStringDirPathField.stringValue = projectPathBefore;
+    }
+    
+    NSString *langKeysBefore = [[NSUserDefaults standardUserDefaults] objectForKey:langKeysKey];
+    if (langKeysBefore.length > 0){
+        self.langKeysField.stringValue = langKeysBefore;
+        NSArray *langKeys = [langKeysBefore componentsSeparatedByString:@","];
+        self.langKeys = langKeys;
+    }
+    
+}
+
+#pragma mark - action
+- (IBAction)excelPathSelected:(NSButton *)sender {
     
     NSOpenPanel *panel = [NSOpenPanel openPanel];
     [panel setCanChooseFiles:YES];//是否能选择文件file
     [panel setCanChooseDirectories:YES];//是否能打开文件夹
     [panel setAllowsMultipleSelection:NO];//是否允许多选file
     [panel  setAllowedFileTypes:@[@"xls"]];
-    NSInteger finded = [panel runModal]; //获取panel的响应
-    if (finded == NSModalResponseOK) {
-       NSArray * urls = [panel URLs];
+    NSInteger founded = [panel runModal]; //获取panel的响应
+    if (founded == NSModalResponseOK) {
+        NSArray * urls = [panel URLs];
         NSString * excelUrl = [[urls firstObject] path];
-        self.ecxelPathField.stringValue = excelUrl;
+        self.excelPathField.stringValue = excelUrl;
         NSLog(@"excel path = %@",excelUrl);
+        
+        [[NSUserDefaults standardUserDefaults] setObject:excelUrl forKey:excelPathStoreKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
     }
 }
 
-- (IBAction)stringFilePathSeleted:(NSButton *)sender {
+- (IBAction)stringFilePathSelected:(NSButton *)sender {
     
     NSOpenPanel *panel = [NSOpenPanel openPanel];
     [panel setCanChooseFiles:NO];//是否能选择文件file
     [panel setCanChooseDirectories:YES];//是否能打开文件夹
     [panel setAllowsMultipleSelection:NO];//是否允许多选file
-    NSInteger finded = [panel runModal]; //获取panel的响应
-    if (finded == NSModalResponseOK)
+    NSInteger founded = [panel runModal]; //获取panel的响应
+    if (founded == NSModalResponseOK)
     {
         NSArray * urls = [panel URLs];
-        NSString * excelUrl = [[urls firstObject] path];
-        self.stringPathField.stringValue = excelUrl;
-        NSLog(@"excel path = %@",excelUrl);
+        NSString * stringUrl = [[urls firstObject] path];
+        self.stringPathField.stringValue = stringUrl;
+        NSLog(@"string path = %@",stringUrl);
+        
+        [[NSUserDefaults standardUserDefaults] setObject:stringUrl forKey:stringPathStoreKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
     }
 }
-- (IBAction)convertBtnCliked:(NSButton *)sender {
-     if(self.ecxelPathField.stringValue.length <= 0)
-     {
-         [self alert:@"请选择 xls 文件"];
-         return;
-     }
-    if (self.stringPathField.stringValue.length <= 0) {
-        [self alert:@"请选择 导出路径"];
+
+- (IBAction)projectStringDirPathSelect:(NSButton *)sender {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseFiles:NO];//是否能选择文件file
+    [panel setCanChooseDirectories:YES];//是否能打开文件夹
+    [panel setAllowsMultipleSelection:NO];//是否允许多选file
+    NSInteger founded = [panel runModal]; //获取panel的响应
+    if (founded == NSModalResponseOK)
+    {
+        self.projectStringDirPathField.enabled = NO;
+        NSArray * urls = [panel URLs];
+        NSString * stringUrl = [[urls firstObject] path];
+        self.projectStringDirPathField.stringValue = stringUrl;
+        NSLog(@"project string path = %@",stringUrl);
+        
+        [[NSUserDefaults standardUserDefaults] setObject:stringUrl forKey:projectResourceKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
+
+- (IBAction)convertButtonClicked:(NSButton *)sender {
+    if(self.excelPathField.stringValue.length <= 0)
+    {
+        [self alert:@"请选择 xls 文件"];
         return;
     }
-    NSLog(@"excel 路径 = %@",self.ecxelPathField.stringValue);
-    DHxlsReader * reader = [DHxlsReader xlsReaderWithPath:self.ecxelPathField.stringValue];
+    if (self.stringPathField.stringValue.length <= 0 && self.projectStringDirPathField.stringValue.length <= 0) {
+        [self alert:@"导出路径 和 项目翻译路径 至少选择一个"];
+        return;
+    }
+    
+    if (self.langKeysField.stringValue.length <= 0){
+        [self alert:@"excel中对应语言的key需要指定（eg: en,es,de)"];
+        return;
+    }
+    
+    NSString *langKeysString = [self.langKeys componentsJoinedByString:@","];
+    [[NSUserDefaults standardUserDefaults] setObject:langKeysString forKey:langKeysKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    NSLog(@"excel 路径 = %@",self.excelPathField.stringValue);
+    DHxlsReader * reader = [DHxlsReader xlsReaderWithPath:self.excelPathField.stringValue];
     if (!reader)
     {
         [self alert:@"文件格式不正确!!!"];
@@ -73,14 +160,18 @@
     
     NSInteger sheetCount = [reader numberOfSheets];
     
-    NSInteger row = [reader rowsForSheetAtIndex:0];
-    NSInteger cellCount = [reader numberOfColsInSheet:0];
+    NSInteger row = [reader rowsForSheetAtIndex:0]; // 1983
+    NSInteger cellCount = [reader numberOfColsInSheet:0]; // 16
     
     NSMutableArray * strings = [[NSMutableArray alloc] init];
     for (NSInteger i = 0; i < row; i++)
     {
         DHcell * keyCell = [reader cellInWorkSheetIndex:1 row:i + 1 col:1];
-        for (NSInteger j = 2; j <=cellCount; j++)
+        if (keyCell.str.length == 0){ // key为空，跳过此翻译
+            continue;
+        }
+        
+        for (NSInteger j = 2; j <= cellCount; j++)
         {
             if (j > strings.count)
             {
@@ -92,9 +183,13 @@
             if (cell.str.length > 0)
             {
                 [str appendFormat:@"\n\"%@\" = ",keyCell.str];
-                NSString * value = cell.str;
-                value = [value stringByReplacingOccurrencesOfString:@"%s" withString:@"%@"];
-                value = [value stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+                NSString * value = cell.str; // 可对取出来的值，进行一些额外处理。
+                //value = [value stringByReplacingOccurrencesOfString:@"%s" withString:@"%@"];
+                //value = [value stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+                if ([value containsString:@"\""]){
+                    value = [value stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+                }
+                value = [self removeSpaceAndNewLine:value];
                 [str appendFormat:@"\"%@\";",value];
             }
             
@@ -102,46 +197,116 @@
     }
     NSLog(@"sheetCount = %ld row = %ld cellCount = %ld",(long)sheetCount,row,cellCount);
     
-    NSLog(@"str0 = %@",strings);
+    // ⚠️special deal for epal. 移除掉备注列
+    if(strings.count > 0){
+        NSMutableArray *tmp = [strings mutableCopy];
+        [tmp removeObjectAtIndex:0];
+        strings = tmp;
+    }
     [self saveToFiles:strings];
 }
 
 - (void)saveToFiles:(NSArray *)strings
 {
+
+    
     for (NSInteger i = 0; i < strings.count; i++)
     {
-        NSString * strPath = [self.stringPathField.stringValue stringByAppendingPathComponent:[NSString stringWithFormat:@"%ld.strings",(long)i]];
-        NSString * s = strings[i];
-        if (s.length > 0)
-        {
-            NSData * data = [s dataUsingEncoding:NSUTF8StringEncoding];
-            NSError * error ;
-           BOOL isSucc = [data writeToURL:[NSURL fileURLWithPath:strPath] options:NSDataWritingAtomic error:&error];
-            if (error)
+        
+        // strPath way 1 ：
+//        NSString *langKey = self.langKeys[i];
+//        // 创建文件文件夹。。放入文件
+//        NSString *newDir = [self.stringPathField.stringValue stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.lproj", langKey]];
+//
+//        if (![[NSFileManager defaultManager] fileExistsAtPath:newDir]){
+//            NSError *createDirError;
+//            [[NSFileManager defaultManager] createDirectoryAtPath:newDir withIntermediateDirectories:YES attributes:nil error:&createDirError];
+//            if (createDirError){
+//                [self alert:[NSString stringWithFormat:@"创建文件夹失败:%@ \n error = %@ ",newDir, createDirError.localizedDescription]];
+//                return;
+//            }
+//        }
+//        NSString * strPath = [newDir stringByAppendingPathComponent:@"Localizable.strings"];
+
+        if (self.stringPathField.stringValue.length > 0){
+            NSString * s = strings[i];
+            if (s.length > 0)
             {
-                NSLog(@"error = %@",[error userInfo]);
+                NSString *stringName = self.langKeys[i];
+                NSString * strPath = [self.stringPathField.stringValue stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.strings", stringName]];
+                NSData * data = [s dataUsingEncoding:NSUTF8StringEncoding];
+                NSError * error ;
+                BOOL writeResult = [data writeToURL:[NSURL fileURLWithPath:strPath] options:NSDataWritingAtomic error:&error];
+                if (error){
+                    NSLog(@"error = %@",[error userInfo]);
+                }
+                if (writeResult){
+                    NSLog(@"写入文件成功");
+                }
+                else{
+                    [self alert:[NSString stringWithFormat:@"写入文件失败:%@ \n error = %@",strPath,error.userInfo]];
+                    NSLog(@"写入文件失败");
+                    return ;
+                }
             }
-            if (isSucc)
+        }
+        
+        if (self.projectStringDirPathField.stringValue.length > 0){
+            NSString * s = strings[i];
+            if (s.length > 0)
             {
-                NSLog(@"写入文件成功");
-            }
-            else
-            {
-                [self alert:[NSString stringWithFormat:@"写入文件失败:%@ \n error = %@",strPath,error.userInfo]];
-                NSLog(@"写入文件失败");
-                return ;
+                NSString *langKey = self.langKeys[i];
+                NSString *langDir = [self.projectStringDirPathField.stringValue stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.lproj", langKey]];
+                NSString *strPath = [langDir stringByAppendingPathComponent:@"Localizable.strings"];
                 
+                NSData * data = [s dataUsingEncoding:NSUTF8StringEncoding];
+                NSError * error ;
+                BOOL writeResult = [data writeToURL:[NSURL fileURLWithPath:strPath] options:NSDataWritingAtomic error:&error];
+                if (error){
+                    NSLog(@"error = %@",[error userInfo]);
+                }
+                if (writeResult){
+                    NSLog(@"写入文件成功");
+                }
+                else{
+                    [self alert:[NSString stringWithFormat:@"写入文件失败:%@ \n error = %@",strPath,error.userInfo]];
+                    NSLog(@"写入文件失败");
+                    return ;
+                }
             }
         }
     }
-    [[NSWorkspace sharedWorkspace] selectFile:nil inFileViewerRootedAtPath:self.stringPathField.stringValue];
+    
+    [self alert:@"操作完成"];
+//    [[NSWorkspace sharedWorkspace] selectFile:nil inFileViewerRootedAtPath:self.stringPathField.stringValue];
 }
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
+    
     // Update the view, if already loaded.
 }
 
+#pragma mark - 不重要 action
+
+- (IBAction)tapOpenExcelDir:(id)sender {
+    if (self.excelPathField.stringValue.length > 0){
+        [[NSWorkspace sharedWorkspace] selectFile:nil inFileViewerRootedAtPath:self.excelPathField.stringValue];
+    }
+}
+- (IBAction)tapOpenStringDir:(id)sender {
+    if (self.stringPathField.stringValue.length > 0){
+        [[NSWorkspace sharedWorkspace] selectFile:nil inFileViewerRootedAtPath:self.stringPathField.stringValue];
+    }
+}
+
+- (IBAction)tapOpenProjectStringDir:(id)sender {
+    if (self.projectStringDirPathField.stringValue.length > 0){
+        [[NSWorkspace sharedWorkspace] selectFile:nil inFileViewerRootedAtPath:self.projectStringDirPathField.stringValue];
+    }
+}
+
+
+#pragma mark - helper
 - (void)alert:(NSString *)msg{
     NSAlert *alert = [[NSAlert alloc] init];
     alert.messageText = @"系统提示:";
@@ -150,9 +315,15 @@
     NSInteger ret = [alert runModal];
     switch(ret){
         default:
-            printf("按钮点击了。\n");
+            printf("点击了OK。\n");
             break;
     }
+}
+
+- (NSString *)removeSpaceAndNewLine:(NSString *)str {
+    NSString *temp = [str.copy stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    NSString *text = [temp stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet ]];
+    return text;
 }
 
 
